4c166b79a1e03ef7cadebb1cfbb7de8ac1a27834
root <root@localhost.localdomain>
2020-10-18 20:32:57 -0400

    FINAL COMMIT

--- README.md
+++
@@ -1,2 +0,0 @@
-# Secure password generator
-# The passwords created from this generator can not be brute forced garunteed
--- models.py
+++
@@ -1,7 +0,0 @@
-from . import db
-
-class Comment(db.mode1):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100))
-    company = db.Column(db.String(100000))
-    
